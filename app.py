76d2dac5cdbb47f611e90721e4ecf9969dadd57e
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-01-13 01:39:21 +0200

    Updated init() from template

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from transformers import pipeline
+from whisper import load_model
+import whisperx
 import torch
+import os
+import wget
+import json
+from omegaconf import OmegaConf
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    global model
+    global whisper_model, alignment_model, metadata
     
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    device = 'cuda' if torch.cuda.is_available() else -1
+    # Large models result in considerably better and more aligned (words, timestamps) mapping. 
+    whisper_model = load_model("large-v2")
+    alignment_model, metadata = whisperx.load_align_model(language_code='en',
+                                                      device=device,
+                                                      model_name = 'WAV2VEC2_ASR_LARGE_LV60K_960H')
+    
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
-    global model
+    global whisper_model, alignment_model, metadata
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
